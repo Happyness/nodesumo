dc0d3122a3cb1b6d5535eafb069d9b5e759a9051
Joel Denke <mail@happyness.se>
2014-03-06 15:09:55 +0100

    search complete

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -25,9 +25,15 @@ angular.module('sumoApp.controllers', []).
             console.log($scope.add_index);
             console.log($scope.add_indextype);
 
+            var variants = $scope.add_variants.split(',');
+            $.each(variants, function(index, value)
+            {
+                variants[index] = $.trim(value);
+            });
+
             var json = {
                 keyword: $scope.add_keyword,
-                variants: [$scope.add_variants],
+                variants: variants,
                 index: $scope.add_index,
                 indexType: $scope.add_indextype
             }
@@ -77,15 +83,55 @@ angular.module('sumoApp.controllers', []).
 
   }).
     controller('searchController', function($scope, $http) {
+        $scope.searchTypes = [{id: 'keyword', label: 'Keyword'},
+            {id: 'variant', label: 'Variant'},
+            {id: 'stopword', label: 'Stopword'}];
+
         $http({
             method: 'GET',
-            url: 'http://localhost:8080/rest/search/synonym/keyword/*/*/*'
+            url: 'http://localhost:8080/rest/search/index'
         }).
 
             success(function (data, status, headers, config) {
-                $scope.result = data;
-            }).
-            error(function (data, status, headers, config) {
-                $scope.result = 'Error!';
+                $scope.indexes = data;
             });
+
+        $http({
+            method: 'GET',
+            url: 'http://localhost:8080/rest/search/indextype'
+        }).
+
+            success(function (data, status, headers, config) {
+                $scope.indexTypes = data;
+            });
+
+        $scope.submit = function()
+        {
+            console.log($scope.search_word);
+            console.log($scope.search_type);
+            console.log($scope.search_index);
+            console.log($scope.search_indextype);
+
+            var index = $scope.search_index;
+            var indexType = $scope.search_indextype;
+            var word = $scope.search_word;
+            var type = $scope.search_type;
+            var uriPart = '';
+
+            if (type == 'variant' || type == 'keyword') {
+                uriPart += '/synonym'
+            }
+
+            $http({
+                method: 'GET',
+                url: 'http://localhost:8080/rest/search' + uriPart + '/' + type + '/' + word + '/' + index + '/' + indexType
+            }).
+
+                success(function (data, status, headers, config) {
+                    $scope.result = data;
+                }).
+                error(function (data, status, headers, config) {
+                    $scope.result = 'Error!';
+                });
+        };
     });
